857a481f3def4322918e770e65092188079af445
Xu Hu <hxtheone@gmail.com>
2015-11-09 23:58:03 +0800

    use portal component to make modal dialog but still to fix bug

--- client/javascripts/modules/Question/components/SaveSelection.react.js
+++ client/javascripts/modules/Question/components/SaveSelection.react.js
@@ -1,11 +1,36 @@
 import $ from 'jquery';
 import React, { Component } from 'react';
+import ReactDom from 'react-dom';
 import { connect } from 'react-redux';
 import { savePaper } from '../../Paper/actions/PaperActions';
 
 class SaveSelection extends Component {
   constructor (props) {
     super(props);
+
+    this._overlayTarget = document.getElementById('modal-area');
+    this._overlay = (
+      <div>
+        <div className='ui small modal'>
+          <div className="header">
+            Save Selection as Paper
+          </div>
+          <div className='content'>
+            <label>Enter Paper Name: </label>
+            <div className='ui action input'>
+              <input type='text' className='paper-name'/>
+            </div>
+          </div>
+          <div className='actions'>
+            <button className='ui teal right labeled icon approve button'>
+              <i className='save icon'></i>
+              Save
+            </button>
+            <button className='ui cancel red button'>Cancel</button>
+          </div>
+        </div>
+      </div>
+    );
 
     this.showConfrimDialog = this.showConfrimDialog.bind(this);
     this.saveInputName = this.saveInputName.bind(this);
@@ -27,30 +52,16 @@ class SaveSelection extends Component {
     this.props.dispatch(savePaper(inputName));
   }
 
+  componentDidMount () {
+    ReactDom.render(this._overlay, this._overlayTarget);
+  }
+
+  componentWillUnmount () {
+    ReactDom.unmountComponentAtNode(this._overlayTarget)
+  }
+
   render () {
-    return (
-      <div>
-        <div className='ui small modal'>
-          <div className="header">
-            Save Selection as Paper
-          </div>
-          <div className='content'>
-            <label>Enter Paper Name: </label>
-            <div className='ui action input'>
-              <input type='text' className='paper-name'/>
-            </div>
-          </div>
-          <div className='actions'>
-            <button className='ui teal right labeled icon approve button'>
-              <i className='save icon'></i>
-              Save
-            </button>
-            <button className='ui cancel red button'>Cancel</button>
-          </div>
-        </div>
-        <button className='ui grey button' onClick={this.showConfrimDialog}>Save as Paper</button>
-      </div>
-    );
+    return <button className='ui grey button' onClick={this.showConfrimDialog}>Save as Paper</button>;
   }
 }
 
